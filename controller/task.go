3feb407b23951e1899e098813801bb355d4a82af
BPCoder <mingyangcoder@163.com>
2025-08-26 11:52:47 +0800

    feat: 增加 标签列表配置

--- controller/task.go
+++ controller/task.go
@@ -4,87 +4,61 @@ import (
 	"goumang-master/db"
 	"goumang-master/errorcode"
 	"goumang-master/global"
+	"sort"
 
+	"github.com/bpcoder16/Chestnut/v2/logit"
 	"github.com/gin-gonic/gin"
 )
 
 type Task struct{}
 
+// Config 获取任务配置相关信息
 func (t *Task) Config(ctx *gin.Context) {
-	var dbNodeList []db.GMNode
-	if err := global.DefaultDB.WithContext(ctx).Where(&db.GMNode{
-		Status: db.StatusEnabled,
-	}).Find(&dbNodeList).Error; err != nil {
+	// 获取标签列表
+	tags, err := t.getTagList(ctx)
+	if err != nil {
 		returnErrJson(ctx, errorcode.ErrServiceException)
 		return
 	}
+
+	// 构建标签列表
+	tagList := buildTagList(tags)
+
 	returnSuccessJson(ctx, gin.H{
-		"typeMethodList": []map[string]any{
-			{"name": db.TaskMethodMap[db.NotChoose], "value": db.NotChoose},
-		},
-		"taskTypeList": []map[string]any{
-			{"name": db.TaskTypeMap[db.NotChoose], "value": db.NotChoose},
-			{"name": db.TaskTypeMap[db.TypeCron], "value": db.TypeCron},
-			{"name": db.TaskTypeMap[db.TypeDuration], "value": db.TypeDuration},
-			{"name": db.TaskTypeMap[db.TypeDurationRandom], "value": db.TypeDurationRandom},
-			{"name": db.TaskTypeMap[db.TypeOneTimeJobStartDateTimes], "value": db.TypeOneTimeJobStartDateTimes},
-		},
-		"taskStatusList": []map[string]any{
-			{"name": db.TaskStatusMap[db.NotChoose], "value": db.NotChoose},
-			{"name": db.TaskStatusMap[db.StatusPending], "value": db.StatusPending},
-			{"name": db.TaskStatusMap[db.StatusEnabled], "value": db.StatusEnabled},
-			{"name": db.TaskStatusMap[db.StatusConfigError], "value": db.StatusConfigError},
-			{"name": db.TaskStatusMap[db.StatusConfigExpired], "value": db.StatusConfigExpired},
-			{"name": db.TaskStatusMap[db.StatusWorkerServiceError], "value": db.StatusWorkerServiceError},
-		},
-		"nodeList": func() []map[string]any {
-			nodeList := make([]map[string]any, 0, len(dbNodeList)+1)
-			nodeList = append(nodeList, map[string]any{
-				"name": "全部", "value": db.NotChoose,
-			})
-			for _, node := range dbNodeList {
-				nodeList = append(nodeList, map[string]any{
-					"name":  node.Title,
-					"value": node.ID,
-				})
-			}
-			return nodeList
-		}(),
+		"tagList": tagList,
 	})
 }
 
-func (t *Task) List(ctx *gin.Context) {
-	var req struct {
-		Page     int    `json:"page" binding:"required,min=1"`
-		PageSize int    `json:"page_size" binding:"required,min=1"`
-		Title    string `json:"title"`
-		Tag      string `json:"tag"`
-		Method   int8   `json:"method"`
-		NodeId   uint64 `json:"node_id"`
-		Status   int8   `json:"status"`
-	}
 
-	if err := paramsValidator(ctx, &req); err != nil {
+
+// getTagList 获取标签列表
+func (t *Task) getTagList(ctx *gin.Context) (tagList []string, err error) {
+	if err = global.DefaultDB.WithContext(ctx).Model(&db.GMTask{}).
+		Where("status != ?", db.StatusDeleted).
+		Distinct("tag").
+		Pluck("tag", &tagList).Error; err != nil {
+		logit.Context(ctx).ErrorW("getTagList.error", err)
 		return
 	}
 
-	where := make([]string, 0, 5)
-	whereParams := make([]any, 0, 5)
-	if len(req.Title) > 0 {
-		where = append(where, "title LIKE ?")
-		whereParams = append(whereParams, "%"+req.Title+"%")
-	}
-	if len(req.Tag) > 0 {
-		where = append(where, "tag LIKE ?")
-		whereParams = append(whereParams, "%"+req.Tag+"%")
-	}
-	if req.Method != 0 {
-		where = append(where, "method = ?")
-		whereParams = append(whereParams, req.Method)
-	}
-	if req.NodeId != 0 {
-		where = append(where, "node_id = ?")
-		whereParams = append(whereParams, req.NodeId)
-	}
+	// 按字母顺序排序
+	sort.Strings(tagList)
+	return
+}
 
+// buildTagList 构建标签列表
+func buildTagList(tags []string) []map[string]any {
+	tagList := make([]map[string]any, 0, len(tags)+1)
+	tagList = append(tagList, map[string]any{
+		"name": "全部", "value": "",
+	}, map[string]any{
+		"name": "无标签", "value": "NoTag",
+	})
+	for _, tag := range tags {
+		tagList = append(tagList, map[string]any{
+			"name":  tag,
+			"value": tag,
+		})
+	}
+	return tagList
 }
